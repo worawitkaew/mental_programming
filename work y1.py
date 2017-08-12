d6f0176db2342944b91c1d82d449cab5e44ecc3b
worawit <mon2350@hotmail.com>
2017-08-12 21:18:49 +0700

    Update

--- work y1.py
+++ work y1.py
@@ -1,4 +1,27 @@
- #! user / 600510577
-f=(float)(input("input temperature in fahrenheit: "))
-c=5*(f-32)/9
-print(f , "degree fahrenheit is ",c,"degree celsius ")
+
+
+
+def functiona(m,x,b):
+    """
+        this function use for create number
+    """
+
+    # this is comment2
+    return m*x+b
+
+class ClassA(object):
+    """
+        description
+    """
+    def __init__(self):
+        print("this is constructor")
+
+if __name__ == '__main__':
+    #! user / 600510577
+    f=(float)(input("input temperature in fahrenheit: "))
+    c=5*(f-32)/9
+    print(f , "degree fahrenheit is ",c,"degree celsius ")
+
+    y = functiona(10,10,0)
+    print(y)
+    print(functiona(10,10,0))
